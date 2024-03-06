160927ad695f9f425c1542ada2334fe88da90b62
Andrii Kvashuk <andremtcp2015a@gmail.com>
2024-03-06 16:35:18 +0000

    Add QuoteListView with the button to go there

--- TCATestApp/Features/Book/EditBookView.swift
+++ TCATestApp/Features/Book/EditBookView.swift
@@ -188,15 +188,16 @@ struct EditBookView: View {
                 TextEditor(text: viewStore.$synopsis)
                     .padding(5)
                     .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color(uiColor: .tertiarySystemFill), lineWidth: 2))
-    //            NavigationLink {
-    //                QuotesListView(book: book)
-    //            } label: {
-    //                let count = book.quotes?.count ?? 0
-    //                Label("^[\(count) Quotes](inflect: true)", systemImage: "quote.opening")
-    //            }
-    //            .buttonStyle(.bordered)
-    //            .frame(maxWidth: .infinity, alignment: .trailing)
-    //            .padding(.horizontal)
+                 Button {
+//                     QuotesListView(book: viewStore.book)
+                     debugPrint("tapped")
+                } label: {
+                    let count = viewStore.book.quotes?.count ?? 0
+                    Label("^[\(count) Quotes](inflect: true)", systemImage: "quote.opening")
+                }
+                .buttonStyle(.bordered)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.horizontal)
 
             }
             .padding()
